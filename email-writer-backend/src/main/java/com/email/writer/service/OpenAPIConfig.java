41dce93ce98835079ef5fd1f7190dbb4e041e69f
Laxminarayana Alekha <laxmishalekha123@gmail.com>
2025-07-13 17:07:24 +0530

    swagger docs

---
+++ email-writer-backend/src/main/java/com/email/writer/service/OpenAPIConfig.java
@@ -0,0 +1,33 @@
+package com.email.writer.service;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class OpenAPIConfig {
+
+    @Value("${api.title}")
+    private String title;
+
+    @Value("${api.description}")
+    private String description;
+
+    @Value("${api.version}")
+    private String version;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .description(description)
+                        .version(version)
+                );
+    }
+}
